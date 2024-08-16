ba45f5418dbdc72f9375c5254aeeddfd25de85f6
LitenPanJun <xiaopanjun233@163.com>
2024-08-16 14:01:35 +0800

    feat: Server controller

--- Controllers/PanelController.cs
+++ Controllers/PanelController.cs
@@ -6,6 +6,8 @@ namespace Minecraft_Server_Console.Controllers
 {
     public class PanelController : Controller
     {
+        private Server server = new("./App_Data/Server");
+
         private readonly ILogger<PanelController> _logger;
 
         public PanelController(ILogger<PanelController> logger)
@@ -15,7 +17,11 @@ namespace Minecraft_Server_Console.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var model = new ConsoleViewModel
+            {
+                TextAreaContent = "等待服务器电波ing"
+            };
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -23,5 +29,29 @@ namespace Minecraft_Server_Console.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        [HttpPost]
+        public IActionResult Index(string action, ConsoleViewModel model)
+        {
+            if (action == "RunServer")
+            {
+                model.TextAreaContent = "";
+                //server.Run("./App_Data/Jre/bin/java.exe", "-jar Server.jar -Xms1G -Xmx16G nogui");
+                return View(model);
+            }
+            if (action == "StopServer")
+            {
+                //server.Stop();
+                return View();
+            }
+            if (action == "KillServer")
+            {
+                //server.Kill();
+                model.TextAreaContent += "\n服务器已强制关闭";
+                return View(model);
+            }
+            else return View();
+        }
+
+
     }
 }
